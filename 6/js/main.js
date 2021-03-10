47ffaf2109cb4c3dff7a8ae46d8f2cb8e4e01eb3
Keks <keks@htmlacademy.ru>
2021-03-10 04:50:02 +0300

    :heavy_check_mark: Сборка #6

--- 6/js/main.js
+++ 6/js/main.js
@@ -1,1 +1,10 @@
-import './preview.js';
+import {createPhotos} from './data.js';
+import {drawPhotos} from './photo.js';
+import {showPreview} from './preview.js';
+
+const photos = createPhotos();
+
+drawPhotos(photos);
+showPreview(photos[13]);
+
+
--- 6/js/preview.js
+++ 6/js/preview.js
@@ -1,34 +1,38 @@
-import {createPhotos} from './data.js';
-import {drawPhotos} from './photo.js';
 import {isEscEvent} from './util.js';
 
-const photos = createPhotos();
-const bodyElement = document.querySelector('body');
+const bodyElement = document.body;
 const preview = document.querySelector('.big-picture');
 const previewCloseButton = preview.querySelector('.big-picture__cancel');
 const commentsCount = preview.querySelector('.social__comment-count');
 const commentLoaderButton = preview.querySelector('.comments-loader');
 const commentsList = document.querySelector('.social__comments');
-const commentsArray = photos.map((currentValue) => {
-  return currentValue.comments;
-});
-
-drawPhotos(photos);
 
 const closePreview = () => {
   preview.classList.add('hidden');
   bodyElement.classList.remove('modal-open');
 
   document.removeEventListener('keydown', onPreviewEscDown);
-}
+};
 
-const openPreview = () => {
+const fillPreview = (photo) => {
+  preview.querySelector('img').src = photo.url;
+  preview.querySelector('.likes-count').textContent = photo.likes;
+  preview.querySelector('.comments-count').textContent = photo.comments.length;
+  preview.querySelector('.social__caption').textContent = photo.description;
+
+  drawComments(photo.comments);
+};
+
+const openPreview = (photo) => {
+  fillPreview(photo);
+
   preview.classList.remove('hidden');
   commentsCount.classList.add('hidden');
   commentLoaderButton.classList.add('hidden');
-  bodyElement.classList.add('modal-open');
 
+  bodyElement.classList.add('modal-open');
   document.addEventListener('keydown', onPreviewEscDown);
+
   previewCloseButton.addEventListener('click', closePreview);
 };
 
@@ -64,30 +68,17 @@ const createComment = (comments) => {
 const drawComments = (comments) => {
   clearComments();
 
-  let commentsFragment = document.createDocumentFragment();
-  comments.forEach((currentValue, index) => {
-    commentsFragment = createComment(comments[index]);
-    commentsList.appendChild(commentsFragment);
+  const commentsFragment = document.createDocumentFragment();
+
+  comments.forEach((_, index) => {
+    commentsFragment.appendChild(createComment(comments[index]));
   });
+
+  commentsList.appendChild(commentsFragment);
 };
 
-const fillPreview = (photo) => {
-  preview.querySelector('img').src = photo.url;
-  preview.querySelector('.likes-count').textContent = photo.likes;
-  preview.querySelector('.comments-count').textContent = photo.comments.length;
-  preview.querySelector('.social__caption').textContent = photo.description;
+const showPreview = (photo) => {
+  openPreview(photo);
 };
 
-const showPreview = () => {
-  const photosElement = document.querySelectorAll('.picture');
-  photosElement.forEach((currentValue, index) => {
-    currentValue.addEventListener('click', (evt) => {
-      evt.preventDefault();
-      openPreview();
-      fillPreview(photos[index]);
-      drawComments(commentsArray[index]);
-    });
-  });
-};
-
-showPreview();
+export {showPreview};
