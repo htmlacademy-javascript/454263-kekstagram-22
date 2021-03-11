3c590db431b56fb5ebd3bde7f78ba0f6744d0ae7
Keks <keks@htmlacademy.ru>
2021-03-12 02:40:17 +0300

    :heavy_check_mark: Сборка #7

---
+++ 7/js/util.js
@@ -0,0 +1,23 @@
+// Функция возвращающая целое число [min, max]
+
+const getInteger = (min, max) => {
+  if (min < 0 || min > max) {
+    min = 0;
+  }
+
+  return Math.floor(Math.random() * (max - min + 1) + min);
+};
+
+// Функция для проверки максимальной длины строки
+
+const getMaxStringWidth = (string, maxWidth) => {
+  return string.length <= maxWidth;
+};
+
+getMaxStringWidth('Функция для проверки максимальной длины строки', 140);
+
+const isEscEvent = (evt) => {
+  return evt.key === ('Escape' || 'Esc');
+};
+
+export {getInteger, isEscEvent};
